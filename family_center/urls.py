e483e1fce3a3058b546b8ed8669a6e2b3a21a18b
George Lee <George Lee>
2017-02-15 00:30:27 -0800

    add social login 3

--- family_center/urls.py
+++ family_center/urls.py
@@ -3,6 +3,10 @@ from rest_framework.routers import DefaultRouter
 
 from .api import FBWebhookViewSet
 
+from django.contrib.auth import views as auth_views
+
+import .views as home_views
+
 router = DefaultRouter()
 
 router.register(r'fb', FBWebhookViewSet, base_name='fb')
@@ -10,7 +14,7 @@ router.register(r'fb', FBWebhookViewSet, base_name='fb')
 urlpatterns = [
 	url(r'^center/', include(router.urls)),
 
-	url(r'^$', family_center_views.home, name='home'),
+	url(r'^$', home_views.home, name='home'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
 ]
