b445db74b69aadc826a11d737e6adee18549f04e
iamgeorgelee <iamgeorgelee@gmail.com>
2017-02-17 16:37:06 -0500

    finally get email

--- yourfamily/settings.py
+++ yourfamily/settings.py
@@ -160,20 +160,22 @@ SOCIAL_AUTH_TWITTER_SECRET = 'whatever'
 SOCIAL_AUTH_FACEBOOK_KEY = 'whatever'  # App ID
 SOCIAL_AUTH_FACEBOOK_SECRET = 'whatever'  # App Secret
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
+    'fields': 'id,name,email', 
+}
 
-
-SOCIAL_AUTH_PIPELINE = (
-	'social_core.pipeline.social_auth.social_details',
-	'social_core.pipeline.social_auth.social_uid',
-	'social_core.pipeline.social_auth.auth_allowed',
-	'social_core.pipeline.social_auth.social_user',
-	'social_core.pipeline.social_auth.associate_by_email',
-	'social_core.pipeline.user.get_username',
-	'social_core.pipeline.user.create_user',
-	'social_core.pipeline.social_auth.associate_user',
-	'social_core.pipeline.social_auth.load_extra_data',
-	'social_core.pipeline.user.user_details',
-)
+# SOCIAL_AUTH_PIPELINE = (
+#	'social_core.pipeline.social_auth.social_details',
+#	'social_core.pipeline.social_auth.social_uid',
+#	'social_core.pipeline.social_auth.auth_allowed',
+#	'social_core.pipeline.social_auth.social_user',
+#	'social_core.pipeline.social_auth.associate_by_email',
+#	'social_core.pipeline.user.get_username',
+#	'social_core.pipeline.user.create_user',
+#	'social_core.pipeline.social_auth.associate_user',
+#	'social_core.pipeline.social_auth.load_extra_data',
+#	'social_core.pipeline.user.user_details',
+#)
 
 # local setting for development.
 try:
