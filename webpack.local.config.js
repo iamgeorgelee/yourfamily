ba58106af95a4ae121071cac37141d924a7d3627
iamgeorgelee <iamgeorgelee@gmail.com>
2017-03-06 22:49:09 -0500

    Going to production

--- webpack.local.config.js
+++ webpack.local.config.js
@@ -3,10 +3,31 @@ var webpack = require('webpack')
 var BundleTracker = require('webpack-bundle-tracker')
 var config = require('./webpack.base.config.js')
 
+var ip = 'localhost'
+
+config.ip = ip
+
+config.entry = {
+  App1: [
+    'webpack-dev-server/client?http://' + ip + ':3000',
+    'webpack/hot/only-dev-server',
+    './reactjs/App1',
+  ],
+}
+
+config.output.publicPath = 'http://' + ip + ':3000' + '/assets/bundles/'
+
 config.devtool = "#eval-source-map"
 
 config.plugins = config.plugins.concat([
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoErrorsPlugin(),
   new BundleTracker({filename: './webpack-stats-local.json'}),
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('development'),
+      'BASE_API_URL': JSON.stringify('https://'+ ip +':8000/yourfamily/'),
+  }}),
 ])
 
 config.module.loaders.push(
